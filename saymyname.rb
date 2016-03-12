a9b6dcf651baa83896c3e798f0a0b742813580a4
Hannah <hannahnotess@gmail.com>
2016-03-11 22:33:06 -0800

    add saymyname.rb

---
+++ saymyname.rb
@@ -0,0 +1,10 @@
+# my first Ruby program.
+# I like listening to '90s hip hop while writing code, so I named it after a Destiny's Child song.
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "Hello, " + name + ". How are you?"
+state = gets.chomp
+
+puts "I am also " + state + " today."
